0e4e6d26ebcc06ee7ca77455217a8dab6fd3e911
sitdikov <platunovab-ext@vip.cbr.ru>
2025-03-06 17:31:08 +0700

    patterns-core добавил направление денег в операциях

--- core/src/main/java/ru/hits/core/repository/AccountRepository.java
+++ core/src/main/java/ru/hits/core/repository/AccountRepository.java
@@ -1,10 +1,21 @@
 package ru.hits.core.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.hits.core.domain.entity.AccountEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<AccountEntity, UUID>, JpaSpecificationExecutor<AccountEntity> {
+
+    @Query(
+            value = "select a.id from account a " +
+                    "where a.user_id = :userId", nativeQuery = true
+    )
+    List<UUID> selectAllIdsByUserId(@Param("userId") UUID userId);
+
 }
