37666e419df05f2e27600ca0fe83145baffa292e
sitdikov <platunovab-ext@vip.cbr.ru>
2025-03-09 19:36:41 +0700

    patterns-core fix пополнение баланса после взятия кредита

--- core/src/main/java/ru/hits/core/config/RabbitMQConfig.java
+++ core/src/main/java/ru/hits/core/config/RabbitMQConfig.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -48,8 +50,25 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public MessageListenerAdapter listenerAdapter(RabbitMQListener listener) {
-        return new MessageListenerAdapter(listener, "receiveMessage");
+    public Binding loanRequestBinding(Queue loanRequestQueue, DirectExchange loanExchange) {
+        return BindingBuilder.bind(loanRequestQueue).to(loanExchange).with(LOAN_REQUEST_QUEUE);
     }
 
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RabbitMQListener listener) {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        MessageListenerAdapter adapter = new MessageListenerAdapter(listener, "receiveMessage");
+        adapter.setMessageConverter(converter);
+        return adapter;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer loanRequestContainer(ConnectionFactory connectionFactory,
+                                                               MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(LOAN_REQUEST_QUEUE);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
 }
--- core/src/main/java/ru/hits/core/service/impl/RabbitMQListener.java
+++ core/src/main/java/ru/hits/core/service/impl/RabbitMQListener.java
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Message;
 import org.springframework.stereotype.Component;
 import ru.hits.core.domain.dto.loan.LoanRequest;
 import ru.hits.core.domain.dto.operation.OperationRequestBody;
 import ru.hits.core.domain.enums.OperationTypeEnum;
 import ru.hits.core.service.OperationService;
 
-import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 @Slf4j
@@ -21,30 +23,42 @@ public class RabbitMQListener {
     private final OperationService operationService;
     private final ObjectMapper objectMapper;
 
-    public void receiveMessage(Message message) throws JsonProcessingException {
-        String messageBody = new String(message.getBody(), StandardCharsets.UTF_8);
-
+    public void receiveMessage(LinkedHashMap<String, Object> messageMap) {
         System.out.println("Получено сообщение из RabbitMQ:");
-        System.out.println("Body: " + messageBody);
+        System.out.println("Body: " + messageMap);
 
-        LoanRequest loanRequest = null;
         try {
-            loanRequest = objectMapper.readValue(messageBody, LoanRequest.class);
+            var request = LoanRequest.builder()
+                    .accountId(getUUID(messageMap, "AccountId"))
+                    .amount(getFloat(messageMap, "Amount"))
+                    .build();
+
+            operationService.createOperation(
+                    null,
+                    new OperationRequestBody(
+                            null,
+                            request.getAccountId(),
+                            request.getAmount(),
+                            null,
+                            OperationTypeEnum.REPLENISHMENT
+                    )
+            );
         } catch (Exception e) {
-            log.error(e.getMessage());
-            return;
+            log.error("Ошибка обработки сообщения: " + e.getMessage());
         }
+    }
 
-        operationService.createOperation(
-                null,
-                new OperationRequestBody(
-                        loanRequest.getAccountId(),
-                        null,
-                        loanRequest.getAmount(),
-                        null,
-                        OperationTypeEnum.REPLENISHMENT
-                )
-        );
+    public static UUID getUUID(Map<String, Object> map, String key) {
+        return Optional.ofNullable(map.get(key))
+                .map(Object::toString)
+                .map(UUID::fromString)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid UUID for key: " + key));
+    }
 
+    public static float getFloat(Map<String, Object> map, String key) {
+        return Optional.ofNullable(map.get(key))
+                .map(Object::toString)
+                .map(Float::parseFloat)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid float for key: " + key));
     }
 }
