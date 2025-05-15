74dc64ac8e9672d75efeaa99d0a041f5b52698df
sitdikov <SitdikovYUM-ext@vip.cbr.ru>
2025-05-15 18:26:26 +0700

    patterns-core уведомления fix

--- core/src/main/java/ru/hits/core/config/FirebaseConfig.java
+++ core/src/main/java/ru/hits/core/config/FirebaseConfig.java
@@ -8,21 +8,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
-        
-        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
-                .setCredentials(googleCredentials)
+        InputStream serviceAccount =
+                new ClassPathResource("patterns2-firebase-adminsdk-fbsvc-ec0a960343.json").getInputStream();
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
-        
-        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "patterns2");
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
         return FirebaseMessaging.getInstance(app);
     }
 }
