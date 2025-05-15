8de2865d923f8575d1dae3633cae61916fbd1ef0
sitdikov <SitdikovYUM-ext@vip.cbr.ru>
2025-05-15 20:50:36 +0700

    patterns-core уведомления fix

--- core/src/main/java/ru/hits/core/config/FirebaseConfig.java
+++ core/src/main/java/ru/hits/core/config/FirebaseConfig.java
@@ -4,27 +4,38 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        InputStream serviceAccount =
-                new ClassPathResource("patterns2-firebase-adminsdk-fbsvc-ec0a960343.json").getInputStream();
+    @Value("${firebase.credentials.path}")
+    private String firebaseConfigPath;
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        Resource resource = resourceLoader.getResource(firebaseConfigPath);
+        InputStream serviceAccount = resource.getInputStream();
+
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccount);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(googleCredentials)
                 .build();
 
-        FirebaseApp app = FirebaseApp.initializeApp(options);
+        FirebaseApp app = FirebaseApp.initializeApp(options, "patterns2");
         return FirebaseMessaging.getInstance(app);
     }
 }
