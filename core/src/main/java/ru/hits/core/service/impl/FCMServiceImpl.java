405d96a854f3e8add1eedd668e8bb096ade8f985
sitdikov <SitdikovYUM-ext@vip.cbr.ru>
2025-05-20 17:29:57 +0700

    patterns-core test

--- core/src/main/java/ru/hits/core/service/impl/FCMServiceImpl.java
+++ core/src/main/java/ru/hits/core/service/impl/FCMServiceImpl.java
@@ -24,6 +24,7 @@ public class FCMServiceImpl implements FCMService {
 
         if (fcmToken != null) {
             fcmToken.setFcm(FCMToken);
+            fcmToken.setIsManager(isManager);
             fcmRepository.save(fcmToken);
             log.info("Сохранение токена {}", fcmToken);
             return;
