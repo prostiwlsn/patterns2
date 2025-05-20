53f8d44cab322a818315e3c9e387eb14d96ad2c8
sitdikov <SitdikovYUM-ext@vip.cbr.ru>
2025-05-20 23:02:23 +0700

    test

--- gateway/src/main/java/ru/hits/gateway/controller/FallbackController.java
+++ gateway/src/main/java/ru/hits/gateway/controller/FallbackController.java
@@ -3,29 +3,25 @@ package ru.hits.gateway.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FallbackController {
-    
-    @GetMapping("/fallback/user")
+
+    @RequestMapping(path = "/fallback/user", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
     public ResponseEntity<String> userFallback() {
-        return ResponseEntity
-            .status(503)
-            .body("User Service временно недоступен. Попробуйте позже.");
+        return ResponseEntity.status(503).body("User Service временно недоступен");
     }
 
-    @GetMapping("/fallback/core")
+    @RequestMapping(path = "/fallback/core", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
     public ResponseEntity<String> coreFallback() {
-        return ResponseEntity
-            .status(503)
-            .body("Core Service временно недоступен. Попробуйте позже.");
+        return ResponseEntity.status(503).body("Core Service временно недоступен");
     }
 
-    @GetMapping("/fallback/loan")
+    @RequestMapping(path = "/fallback/loan", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
     public ResponseEntity<String> loanFallback() {
-        return ResponseEntity
-                .status(503)
-                .body("Loan Service временно недоступен. Попробуйте позже.");
+        return ResponseEntity.status(503).body("Loan Service временно недоступен");
     }
 }
